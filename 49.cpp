af4e24623ccd35b74ea525302edd98aaa895402d
AnupamAwasthi2004 <2k22.cse.2213009@gmail.com>
2024-02-13 23:29:57 +0530

    Inorder Traversal

---
+++ 49.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    // Function to return a list containing the inorder traversal of the tree.
+    vector<int> ans;
+    vector<int> inOrder(Node* root) {
+        // Your code here
+        if(root==NULL) return {};
+        else {
+            inOrder(root->left);
+            ans.push_back(root->data);
+            inOrder(root->right);
+        }
+        return ans;
+    }
+};
