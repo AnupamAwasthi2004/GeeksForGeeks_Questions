1cdce882ba5f2f97a3364652199dca25d7b4f6ec
AnupamAwasthi2004 <2k22.cse.2213009@gmail.com>
2024-02-15 15:41:47 +0530

    Exceptionally odd

---
+++ 51.cpp
@@ -0,0 +1,16 @@
+class Solution{   
+public:
+    int getOddOccurrence(int arr[], int n) {
+        // code here
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(mp[arr[i]]%2!=0)
+            return arr[i];
+        }
+        return {};
+            }
+};
